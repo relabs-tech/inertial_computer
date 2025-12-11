7068573689dea049d14cbded84ed140a857f653a
dalarub <daniel@relabs.io>
2025-12-11 20:41:13 +0000

    Added Right IMU

--- internal/sensors/imu.go
+++ internal/sensors/imu.go
@@ -22,16 +22,9 @@ func ReadLeftIMURaw() (imu_raw.IMURaw, error) {
 
 // TODO: replace with real right IMU + mag read
 func ReadRightIMURaw() (imu_raw.IMURaw, error) {
-	return imu_raw.IMURaw{
-		Source: "right",
-		Ax:     0,
-		Ay:     0,
-		Az:     0,
-		Gx:     0,
-		Gy:     0,
-		Gz:     0,
-		Mx:     0,
-		My:     0,
-		Mz:     0,
-	}, nil
+	src, err := NewIMUSourceRight()
+	if err != nil {
+		return imu_raw.IMURaw{}, err
+	}
+	return src.ReadRaw()
 }
